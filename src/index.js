4318d2cebf6598c330ce8fc08af9b4c160fdad76
Morgan <morgan.nkosi@umuzi.org>
2019-11-08 17:07:59 +0200

    moved port

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@
 let express =require('express');
 let app= express();
 const path =require('path')
+const PORT= 8080;
 app.use(express.static('NodeApp'));
 
 app.get(express.static(path.join(__dirname ,__filename)))
-const PORT= 8080;
+
 
 app.listen(PORT,function(){console.log(`Server Has Started on %s`,PORT)});
